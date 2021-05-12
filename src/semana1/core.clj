aa58bf72057b37e9da530a99303f165773d81418
Josimar Assuncao <josimar.assuncao@nubank.com.br>
2021-05-12 16:17:01 -0300

    adds data as reference for customers

--- src/semana1/core.clj
+++ src/semana1/core.clj
@@ -1,5 +1,6 @@
 (ns semana1.core
-  (:require [semana1.creditcard.db.db :as cc.db]))
+  (:require [semana1.creditcard.db.db :as cc.db]
+            [semana1.customer.db.db :as cust.db]))
 
 (defn foo
   "I don't do a whole lot."
@@ -8,7 +9,8 @@
 
 (defn start []
   "starts environment"
-  (cc.db/init-data))
+  (cc.db/init-data)
+  (cust.db/init-data))
 
 (defn -main [& args]
   (println "starting service...")
--- src/semana1/customer/db/db.clj
+++ src/semana1/customer/db/db.clj
@@ -1,1 +1,20 @@
 (ns semana1.customer.db.db)
+
+(def list-of-customers [])
+
+(def default-list [
+                   {:customer-id 301, :name "Angelo", :email "angelo@email.com"}
+                   {:customer-id 411, :name "Maria", :email "maria@email.com"}
+                   {:customer-id 831, :name "Jose", :email "jose@email.com"}
+                   {:customer-id 200, :name "Marta", :email "marta@email.com"}
+                   {:customer-id 234, :name "Hellen", :email "hellen@email.com"}
+                   {:customer-id 235, :name "Erika", :email "erika@email.com"}
+                   ])
+
+(defn get-all-customers []
+  "returns the whole list of customers"
+  list-of-customers)
+
+(defn init-data []
+  "starts the data to test the movements"
+  (def list-of-customers default-list))
