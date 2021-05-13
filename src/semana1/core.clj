8f91d0243e65e6c7eea83287277ad8b511727bd6
Josimar Assuncao <josimar.assuncao@nubank.com.br>
2021-05-13 15:42:54 -0300

    adds partial treatment for expense per client for a certain month

--- src/semana1/core.clj
+++ src/semana1/core.clj
@@ -10,6 +10,7 @@
   (o.db/init-data)
   (println "dbs started"))
 
+;; Merging all data in a unified list
 (defn append-card-info
   [order]
   (->> order
@@ -37,23 +38,148 @@
   []
   (->> (cust.db/get-all-customers)
        (map append-orders-info)
-       )
+       (sort-by :name))
   )
 
 (def full-list (merge-all-data))
 
-(println "\ncliente 200")
+;; Getting data from one customer
+(println "\nCustomer 200")
 (println (->> full-list
               (filter #(= (:customer-id %) 200))))
 
-(println "\ncliente 201")
+(println "\nCostumer 201")
 (println (->> full-list
               (filter #(= (:customer-id %) 201))))
 
-(println "\ncliente 831")
+(println "\nCostumer 831")
 (println (->> full-list
               (filter #(= (:customer-id %) 831))))
 
-(defn main [& args]
+;; Total per customer
+(println "\nTotal spent per customer")
+(defn resume-customer
+  [customer]
+  ;(println "parametro - " customer)
+  (fn [value] {:customer-id (:customer-id customer) :name (:name customer) :total value})
+  )
+
+(defn total-per-customer
+  [customer]
+  ;(println customer)
+  (->> (:orders customer [])
+       (map #(:total-price % 0))
+       (reduce +)
+       ;(println)
+       ((resume-customer customer))
+       ))
+
+(def customer-total-report
+  (->> full-list
+       (map total-per-customer)
+       ;(println)
+       ))
+
+(println customer-total-report)
+
+;; Orders per category
+(println "\nAll orders summed per category")
+(defn resume-category
+  [category]
+  (fn [value] {:category category :total value})
+  )
+
+(defn sum-categories
+  [[key list]]
+  (->> list
+       (map :total-price)
+       (reduce +)
+       ((resume-category key))
+       ))
+
+(def category-total-report
+  (->> full-list
+       (map :orders)
+       (flatten)
+       (#(group-by :category %))
+       (map sum-categories)
+       (sort-by :category)
+       ))
+(println category-total-report)
+
+;; Amount spent in a month Y by customer X
+(defn get-month-from-order
+  [order-info]
+  (subs (:bought_at order-info) 0 7))
+
+(defn only-month-total
+  [month]
+  ;(println month)
+  (fn [total] {:month month, :total total})
+  )
+
+(defn resume-monthly-customer-expense
+  [[month orders]]
+  ;((println month "-" orders))
+  (->> orders
+       ;(flatten)
+       (map #(:total-price % 0))
+       (flatten)
+       ;(#(println month "-" (class %) "-" %))
+       (reduce +)
+       ;(println)
+       ((only-month-total month))
+       ))
+
+(defn resume-customer-months
+  [customer]
+  (fn [months-data]
+    {
+     :customer-id (:customer-id customer)
+     :name (:name customer)
+     :months months-data
+     }))
+
+(defn organize-expenses-per-month
+  [customer]
+  (->> (:orders customer)
+       (group-by get-month-from-order)
+       (map resume-monthly-customer-expense)
+       ((resume-customer-months customer))
+       ))
+
+(defn amount-spent-per-month-per-customer
+  []
+  (->> full-list
+       (map organize-expenses-per-month)
+       ))
+
+(def spent-per-month (amount-spent-per-month-per-customer))
+(println "\nTotal spent per month per customer")
+(println spent-per-month)
+
+(defn get-customer-month-expense
+  [cust-id] (
+       ->> spent-per-month
+           (filter #(= (:customer-id %) cust-id))
+     ))
+
+(println (get-customer-month-expense 301))
+(println (get-customer-month-expense 235))
+
+(defn get-month-value-for-customer
+  [cust-id month]
+  (->> (get-customer-month-expense cust-id)
+       (:months)
+       (filter #(= (:month %) month))
+       ))
+
+;(println (get-customer-month-expense 411))
+(println (get-month-value-for-customer 411 "2021-04"))
+
+;; Search for orders by establishment and by amount of the order
+
+
+(defn -main [& args]
   (println "starting service...")
   (start-dbs!))
