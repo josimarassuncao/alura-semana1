3be42d6528e52f0ae57fd63631e33d1efe696713
Josimar Assuncao <josimar.assuncao@nubank.com.br>
2021-06-03 18:26:12 -0300

    adds schema and kafka dependencies to the project

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.datomic/datomic-pro "1.0.6269"]]
-  :repl-options {:init-ns semana1.core})
+                 [com.datomic/datomic-pro "1.0.6269"]
+                 [org.apache.kafka/kafka-clients "2.7.0"]
+                 [prismatic/schema "1.1.12"]]
+  :repl-options {:init-ns store.core})
